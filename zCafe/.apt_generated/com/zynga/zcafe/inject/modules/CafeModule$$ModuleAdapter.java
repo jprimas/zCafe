9dd842920a488b50c5fca40de5fb0126e99c89b0
JPrimas <joshuaprimas@gmail.com>
2013-11-08 11:24:58 -0800

    trying to animate listview population

--- zCafe/.apt_generated/com/zynga/zcafe/inject/modules/CafeModule$$ModuleAdapter.java
+++ zCafe/.apt_generated/com/zynga/zcafe/inject/modules/CafeModule$$ModuleAdapter.java
@@ -26,36 +26,36 @@ public final class CafeModule$$ModuleAdapter extends ModuleAdapter<CafeModule> {
    */
   @Override
   public void getBindings(Map<String, Binding<?>> map) {
-    map.put("com.zynga.zcafe.events.Producers", new ProvideProducersProvidesAdapter(module));
-    map.put("com.zynga.zcafe.inject.modules.CafeModule$MainThreadBus", new ProvideMainThreadBusProvidesAdapter(module));
     map.put("com.zynga.zcafe.services.CafeService", new ProvideCafeServiceProvidesAdapter(module));
+    map.put("com.zynga.zcafe.inject.modules.CafeModule$MainThreadBus", new ProvideMainThreadBusProvidesAdapter(module));
     map.put("com.zynga.zcafe.CafeApplication", new ProvideApplicationProvidesAdapter(module));
+    map.put("com.zynga.zcafe.events.Producers", new ProvideProducersProvidesAdapter(module));
   }
 
   /**
-   * A {@code Binder<com.zynga.zcafe.events.Producers>} implementation which satisfies
+   * A {@code Binder<com.zynga.zcafe.services.CafeService>} implementation which satisfies
    * Dagger's infrastructure requirements including:
    *
-   * Being a {@code Provider<com.zynga.zcafe.events.Producers>} and handling creation and
+   * Being a {@code Provider<com.zynga.zcafe.services.CafeService>} and handling creation and
    * preparation of object instances.
    */
-  public static final class ProvideProducersProvidesAdapter extends Binding<com.zynga.zcafe.events.Producers>
-      implements Provider<com.zynga.zcafe.events.Producers> {
+  public static final class ProvideCafeServiceProvidesAdapter extends Binding<com.zynga.zcafe.services.CafeService>
+      implements Provider<com.zynga.zcafe.services.CafeService> {
     private final CafeModule module;
 
-    public ProvideProducersProvidesAdapter(CafeModule module) {
-      super("com.zynga.zcafe.events.Producers", null, IS_SINGLETON, "com.zynga.zcafe.inject.modules.CafeModule.provideProducers()");
+    public ProvideCafeServiceProvidesAdapter(CafeModule module) {
+      super("com.zynga.zcafe.services.CafeService", null, IS_SINGLETON, "com.zynga.zcafe.inject.modules.CafeModule.provideCafeService()");
       this.module = module;
       setLibrary(false);
     }
 
     /**
      * Returns the fully provisioned instance satisfying the contract for
-     * {@code Provider<com.zynga.zcafe.events.Producers>}.
+     * {@code Provider<com.zynga.zcafe.services.CafeService>}.
      */
     @Override
-    public com.zynga.zcafe.events.Producers get() {
-      return module.provideProducers();
+    public com.zynga.zcafe.services.CafeService get() {
+      return module.provideCafeService();
     }
   }
 
@@ -87,33 +87,6 @@ public final class CafeModule$$ModuleAdapter extends ModuleAdapter<CafeModule> {
   }
 
   /**
-   * A {@code Binder<com.zynga.zcafe.services.CafeService>} implementation which satisfies
-   * Dagger's infrastructure requirements including:
-   *
-   * Being a {@code Provider<com.zynga.zcafe.services.CafeService>} and handling creation and
-   * preparation of object instances.
-   */
-  public static final class ProvideCafeServiceProvidesAdapter extends Binding<com.zynga.zcafe.services.CafeService>
-      implements Provider<com.zynga.zcafe.services.CafeService> {
-    private final CafeModule module;
-
-    public ProvideCafeServiceProvidesAdapter(CafeModule module) {
-      super("com.zynga.zcafe.services.CafeService", null, IS_SINGLETON, "com.zynga.zcafe.inject.modules.CafeModule.provideCafeService()");
-      this.module = module;
-      setLibrary(false);
-    }
-
-    /**
-     * Returns the fully provisioned instance satisfying the contract for
-     * {@code Provider<com.zynga.zcafe.services.CafeService>}.
-     */
-    @Override
-    public com.zynga.zcafe.services.CafeService get() {
-      return module.provideCafeService();
-    }
-  }
-
-  /**
    * A {@code Binder<com.zynga.zcafe.CafeApplication>} implementation which satisfies
    * Dagger's infrastructure requirements including:
    *
@@ -137,6 +110,33 @@ public final class CafeModule$$ModuleAdapter extends ModuleAdapter<CafeModule> {
     @Override
     public com.zynga.zcafe.CafeApplication get() {
       return module.provideApplication();
+    }
+  }
+
+  /**
+   * A {@code Binder<com.zynga.zcafe.events.Producers>} implementation which satisfies
+   * Dagger's infrastructure requirements including:
+   *
+   * Being a {@code Provider<com.zynga.zcafe.events.Producers>} and handling creation and
+   * preparation of object instances.
+   */
+  public static final class ProvideProducersProvidesAdapter extends Binding<com.zynga.zcafe.events.Producers>
+      implements Provider<com.zynga.zcafe.events.Producers> {
+    private final CafeModule module;
+
+    public ProvideProducersProvidesAdapter(CafeModule module) {
+      super("com.zynga.zcafe.events.Producers", null, IS_SINGLETON, "com.zynga.zcafe.inject.modules.CafeModule.provideProducers()");
+      this.module = module;
+      setLibrary(false);
+    }
+
+    /**
+     * Returns the fully provisioned instance satisfying the contract for
+     * {@code Provider<com.zynga.zcafe.events.Producers>}.
+     */
+    @Override
+    public com.zynga.zcafe.events.Producers get() {
+      return module.provideProducers();
     }
   }
 }
--- zCafe/src/com/zynga/zcafe/adapters/MenuAdapter.java
+++ zCafe/src/com/zynga/zcafe/adapters/MenuAdapter.java
@@ -1,5 +1,6 @@
 package com.zynga.zcafe.adapters;
 
+import static com.nineoldandroids.view.ViewPropertyAnimator.animate;
 import java.util.ArrayList;
 
 import android.content.Context;
@@ -21,70 +22,73 @@ import com.zynga.zcafe.fragments.OrderFormFragment;
 import com.zynga.zcafe.models.MenuItem;
 
 public class MenuAdapter extends ArrayAdapter<MenuItem> {
-  
 
-  Context context;
-  FragmentManager fragmentManager;
-  Fragment fragment;
 
-  public MenuAdapter(Fragment fragment, FragmentManager fragmentManager, Context context,
-      ArrayList<com.zynga.zcafe.models.MenuItem> items) {
-    super(context, 0, items);
-    this.context = context;
-    this.fragmentManager = fragmentManager;
-    this.fragment = fragment;
-    CafeApplication.getObjectGraph().inject(this);
-  }
+	Context context;
+	FragmentManager fragmentManager;
+	Fragment fragment;
 
-  public void push(ArrayList<MenuItem> items) {
-    clear();
-    addAll(items);
-  }
+	public MenuAdapter(Fragment fragment, FragmentManager fragmentManager, Context context,
+			ArrayList<com.zynga.zcafe.models.MenuItem> items) {
+		super(context, 0, items);
+		this.context = context;
+		this.fragmentManager = fragmentManager;
+		this.fragment = fragment;
+		CafeApplication.getObjectGraph().inject(this);
+	}
 
-  @Override
-  public View getView(final int position, View convertView, ViewGroup parent) {
-    ViewHolder holder;
-    View view = convertView;
-    if (view == null) {
-      LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(
-          Context.LAYOUT_INFLATER_SERVICE);
-      view = inflater.inflate(R.layout.menu_item_detail, null);
-      holder = new ViewHolder();
-      holder.ivPhoto = (ImageView) view.findViewById(R.id.ivItemPhoto);
-      holder.tvTitle = (TextView) view.findViewById(R.id.tvItemTitle);
-      holder.tvDescription = (TextView) view.findViewById(R.id.tvItemDescription);
-      holder.bOrder = (Button) view.findViewById(R.id.bItemOrder);
-      view.setTag(holder);
-    } else {
-      holder = (ViewHolder) view.getTag();
-    }
+	public void push(ArrayList<MenuItem> items) {
+		clear();
+		addAll(items);
+	}
 
-    MenuItem item = getItem(position);
-    ImageLoader.getInstance().displayImage(item.getThumbImageUrl(), holder.ivPhoto);
-    holder.tvTitle.setText(item.getTitle());
-    holder.tvDescription.setText(item.getDescription());
-    holder.bOrder.setOnClickListener(new View.OnClickListener() {
+	@Override
+	public View getView(final int position, View convertView, ViewGroup parent) {
+		ViewHolder holder;
+		View view = convertView;
+		if (view == null) {
+			LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(
+					Context.LAYOUT_INFLATER_SERVICE);
+			view = inflater.inflate(R.layout.menu_item_detail, null);
+			holder = new ViewHolder();
+			holder.ivPhoto = (ImageView) view.findViewById(R.id.ivItemPhoto);
+			holder.tvTitle = (TextView) view.findViewById(R.id.tvItemTitle);
+			holder.bOrder = (Button) view.findViewById(R.id.bItemOrder);
+			view.setTag(holder);
+		} else {
+			holder = (ViewHolder) view.getTag();
+		}
 
-      @Override
-      public void onClick(View v) {
-        FragmentTransaction ft = fragmentManager.beginTransaction();
-        ft.detach(fragment);
-        ft.replace(R.id.flFragmentContainer, new OrderFormFragment(),
-            String.valueOf(R.string.order_form_fragment));
-        ft.commit();
-        MenuItem menuItem = getItem(position);
-        CafeApplication app = CafeApplication.getObjectGraph().get(CafeApplication.class);
-        app.getProducers().setMenuItem(menuItem);
-      }
-    });
-    
-    return view;
-  }
+		MenuItem item = getItem(position);
+		ImageLoader.getInstance().displayImage(item.getThumbImageUrl(), holder.ivPhoto);
+		holder.tvTitle.setText(item.getTitle());
+		holder.bOrder.setOnClickListener(new View.OnClickListener() {
 
-  static class ViewHolder {
-    ImageView ivPhoto;
-    TextView tvTitle;
-    TextView tvDescription;
-    Button bOrder;
-  }
+			@Override
+			public void onClick(View v) {
+				FragmentTransaction ft = fragmentManager.beginTransaction();
+				ft.detach(fragment);
+				ft.replace(R.id.flFragmentContainer, new OrderFormFragment(),
+						String.valueOf(R.string.order_form_fragment));
+				ft.commit();
+				MenuItem menuItem = getItem(position);
+				CafeApplication app = CafeApplication.getObjectGraph().get(CafeApplication.class);
+				app.getProducers().setMenuItem(menuItem);
+			}
+		});
+		float y = view.getY(); 
+		System.out.println(y);
+		view.setY(y+500);
+		animate(view).translationY(-y).setDuration(1000);
+
+		return view;
+	}
+
+	static class ViewHolder {
+		ImageView ivPhoto;
+		TextView tvTitle;
+		Button bOrder;
+	}
+
+
 }
