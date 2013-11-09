a67606a47323173582b750f4be9b1dd0913d23db
Vaibhav Sharma <vsharma@zynga.com>
2013-11-08 17:33:36 -0800

    Upload image code

--- zCafe/src/com/zynga/zcafe/services/CafeService.java
+++ zCafe/src/com/zynga/zcafe/services/CafeService.java
@@ -292,5 +292,24 @@ public class CafeService {
     return client;
   }
 
+  public void uploadProfilePic(RequestParams params, String url, HttpEntity entity) {
+      JsonHttpResponseHandler handler = new JsonHttpResponseHandler() {
+
+        @Override
+        public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+          String response = new String(responseBody);
+          Log.i("SUCCESS", response);
+          
+        }
+
+        @Override
+        public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+          Log.i("FAILURE", ""+statusCode);
+        }
+
+      };
+
+      client.post(url, params, handler);
+  }
 
 }
