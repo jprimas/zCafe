7c95945a28ed11d1c60c3e5fbd6909bfebaff0a2
Tony Lee <tony.lie@gmail.com>
2013-11-11 16:22:54 -0800

    receive message ui enhancement

--- zCafe/src/com/zynga/zcafe/adapters/ConnectAdapter.java
+++ zCafe/src/com/zynga/zcafe/adapters/ConnectAdapter.java
@@ -2,24 +2,22 @@ package com.zynga.zcafe.adapters;
 
 import java.util.ArrayList;
 
-import javax.inject.Inject;
-
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.nostra13.universalimageloader.core.ImageLoader;
+import com.zynga.zcafe.CafeApplication;
 import com.zynga.zcafe.R;
-import com.zynga.zcafe.inject.modules.CafeModule.MainThreadBus;
 import com.zynga.zcafe.models.Message;
-import com.zynga.zcafe.services.CafeService;
 
 public class ConnectAdapter extends ArrayAdapter<Message> {
 
@@ -40,11 +38,12 @@ public class ConnectAdapter extends ArrayAdapter<Message> {
   public View getView(final int position, final View convertView, final ViewGroup parent) {
     ViewHolder holder;
     View view = convertView;
+    LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(
+            Context.LAYOUT_INFLATER_SERVICE);
     if (view == null) {
-      LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(
-          Context.LAYOUT_INFLATER_SERVICE);
       view = inflater.inflate(R.layout.connect_detail, null);
       holder = new ViewHolder();
+      holder.layout = (LinearLayout)view.findViewById(R.id.llConnectDetailLayout);
       holder.ivPhotoUrl = (ImageView) view.findViewById(R.id.ivConnectDetailPhotoUrl);
       holder.tvMessage = (TextView) view.findViewById(R.id.tvConnectDetailMessage);
       holder.tvName = (TextView) view.findViewById(R.id.tvConnectDetailName);
@@ -56,6 +55,10 @@ public class ConnectAdapter extends ArrayAdapter<Message> {
 
     final Message message = getItem(position);
     ImageLoader.getInstance().displayImage(message.getPhotoUrl(), holder.ivPhotoUrl);
+    if (message.getName().equals(CafeApplication.getInstance().getProfile().getName())) {
+      holder.layout.setBackgroundResource(R.drawable.connect_list_msg_receive);
+    }
+
     holder.tvMessage.setText(message.getMessage());
     holder.tvName.setText(message.getName());
     holder.tvDate.setText(message.getDate());
@@ -64,6 +67,7 @@ public class ConnectAdapter extends ArrayAdapter<Message> {
   }
 
   static class ViewHolder {
+    LinearLayout layout;
     ImageView ivPhotoUrl;
     TextView tvName;
     TextView tvDate;
